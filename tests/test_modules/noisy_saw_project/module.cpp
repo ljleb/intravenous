5579073b0ebbf49a8c1c0096aa8e5ab5e736b24e
ljleb <lebel.louisjacob@gmail.com>
2026-03-26 20:09:01 -0400

    rename

--- tests/test_modules/noisy_saw_project/module.cpp
+++ tests/test_modules/noisy_saw_project/module.cpp
@@ -5,76 +5,84 @@
 #include "basic_nodes/buffers.h"
 #include "juce_vst_wrapper.h"
 
-namespace iv::modules {
-    inline void noise_voice(GraphBuilder& g)
-    {
-        auto const dt = g.input("dt", 1.0);
+inline void noise_voice(iv::GraphBuilder& g)
+{
+    using namespace iv;
+    auto const dt = g.input("dt", 1.0);
 
-        auto const level_knob = 1.5;
-        auto const lo_pass_knob = 1.0;
-        auto const hi_pass_knob = 1.0;
-        auto const generator = g.node<DeterministicUniformAESNoise>();
-        auto const lo_pass = g.node<SimpleIirLowPass>();
-        auto const hi_pass = g.node<SimpleIirHighPass>();
+    auto const level_knob = 1.5;
+    auto const lo_pass_knob = 1.0;
+    auto const hi_pass_knob = 1.0;
+    auto const generator = g.node<DeterministicUniformAESNoise>();
+    auto const lo_pass = g.node<SimpleIirLowPass>();
+    auto const hi_pass = g.node<SimpleIirHighPass>();
 
-        auto const u_to_n_knob = 0.0;
-        auto const u_to_n = g.node<UniformToGaussian>(0.0, 0.5);
-        auto const u_to_c = g.node<UniformToCauchy>(0.0, 0.01);
-        auto const interp = g.node<Interpolation>();
+    auto const u_to_n_knob = 0.0;
+    auto const u_to_n = g.node<UniformToGaussian>(0.0, 0.5);
+    auto const u_to_c = g.node<UniformToCauchy>(0.0, 0.01);
+    auto const interp = g.node<Interpolation>();
 
-        generator({ {"min", -1}, {"max", 1} });
+    generator({ {"min", -1}, {"max", 1} });
 
-        u_to_c(generator);
-        u_to_n(generator);
+    u_to_c(generator);
+    u_to_n(generator);
 
-        interp(u_to_n, u_to_c, u_to_n_knob);
+    interp(u_to_n, u_to_c, u_to_n_knob);
 
-        lo_pass(interp, lo_pass_knob, dt);
-        hi_pass(lo_pass, hi_pass_knob, dt);
+    lo_pass(interp, lo_pass_knob, dt);
+    hi_pass(lo_pass, hi_pass_knob, dt);
 
-        g.outputs(hi_pass * level_knob);
-    }
-
-    inline void noisy_saw_project(ModuleContext const& context)
-    {
-        GraphBuilder& g = context.builder();
-        ModuleSystem const& system = context.system();
-        auto voice_module = context.load("iv.test.noisy_saw_voice");
-
-        auto const dt = g.node<ValueSource>(&system.sample_period());
-        SignalRef first_noise;
-        auto const val = juce::vst(g, "thing", "D:\\music\\vst-plugins\\3\\x64\\ValhallaSupermassive.vst3");
-        info(val.node());
-        auto const val_idx = std::array {
-            "l0",
-            "r0",
-        };
-
-        val({
-            {"Mix", 0.4},
-            {"DelayWarp", 0.5},
-            {"Density", 0.5},
-        });
-
-        for (size_t channel = 0; channel < system.render_config().num_channels; ++channel) {
-            auto const noise = g.subgraph(noise_voice);
-            if (channel == 0) {
-                first_noise = noise;
-            }
-
-            auto const voice = g.node(voice_module.builder());
-            auto const shared_noise = g.node<Interpolation>();
-            auto const sink = system.sink(g, channel);
-
-            noise(dt);
-            shared_noise(first_noise, noise, 1.0);
-            voice({ {"noise", shared_noise}, {"dt", dt} });
-            val({{val_idx[channel], voice}});
-            sink(val[val_idx[channel]]);
-        }
-
-        g.outputs();
-    }
+    g.outputs(hi_pass * level_knob);
 }
 
-IV_EXPORT_MODULE("iv.test.noisy_saw_project", iv::modules::noisy_saw_project);
+inline void noisy_saw_project(iv::ModuleContext const& context)
+{
+    using namespace iv;
+    GraphBuilder& g = context.builder();
+    ModuleSystem const& system = context.system();
+    auto voice_module = context.load("iv.test.noisy_saw_voice");
+
+    auto const dt = g.node<ValueSource>(&system.sample_period());
+    SignalRef first_noise;
+
+    auto const valhalla = juce::vst(g, "thing", "D:\\music\\vst-plugins\\3\\x64\\ValhallaSupermassive.vst3");
+    info(valhalla.node());
+    valhalla({
+        {"Mix", 0.004},
+        {"DelayWarp", 1.0},
+        {"Delay_Ms", 0.0001},
+        {"Density", 1.0},
+        {"Feedback", 0.0},
+        // {"Mode", 0.1},
+    });
+
+    auto const val_idx = std::array {
+        "l0",
+        "r0",
+    };
+
+    for (size_t channel = 0; channel < system.render_config().num_channels; ++channel) {
+        auto const noise = g.subgraph(noise_voice);
+        if (channel == 0) {
+            first_noise = noise;
+        }
+
+        auto const voice = g.node(voice_module.builder());
+        auto const shared_noise = g.node<Interpolation>();
+        auto const sink = system.sink(g, channel);
+
+        noise(dt);
+        shared_noise(first_noise, noise, 1.0);
+        valhalla({{val_idx[channel], 1.0 * voice["feedback"].detach(3000)}});
+        voice({
+            {"noise", 0.0*shared_noise},
+            {"dt", dt},
+            {"feedback", valhalla[val_idx[channel]]},
+        });
+        sink(voice["out"]);
+    }
+
+    g.outputs();
+}
+
+IV_EXPORT_MODULE("iv.test.noisy_saw_project", noisy_saw_project);
--- tests/test_modules/noisy_saw_voice/module.cpp
+++ tests/test_modules/noisy_saw_voice/module.cpp
@@ -1,24 +1,26 @@
 #include "module/module.h"
 #include "basic_nodes/shaping.h"
+#include "juce_vst_wrapper.h"
 
-namespace {
-    inline void noisy_saw_voice(iv::ModuleContext const& ctx)
-    {
-        using namespace iv;
-        auto& g = ctx.builder();
-        auto const amplitude = g.input("amplitude", 0.1);
-        auto const frequency = g.input("frequency", 500.0);
-        auto const voice_noise = g.input("noise");
-        auto const dt = g.input("dt", 1.0);
-        auto const reset = g.input("reset", 1.0);
+inline void noisy_saw_voice(iv::ModuleContext const& ctx)
+{
+    using namespace iv;
+    auto& g = ctx.builder();
+    auto const amplitude = g.input("amplitude", 0.1);
+    auto const frequency = g.input("frequency", 500.0);
+    auto const voice_noise = g.input("noise");
+    auto const dt = g.input("dt", 1.0);
+    auto const feedback = g.input("feedback", 1.0);
 
-        auto const integrator = g.node<Integrator>();
-        auto const warper = g.node<Warper>();
+    auto const integrator = g.node<Integrator>();
+    auto const warper = g.node<Warper>();
 
-        integrator(warper["aliased"].detach() * reset, frequency * 2.0, dt);
-        warper(integrator + voice_noise);
-        g.outputs(warper["anti_aliased"] * amplitude);
-    }
+    integrator(feedback, frequency * 2.0, dt);
+    warper(integrator + voice_noise);
+    g.outputs({
+        {"out", warper["anti_aliased"] * amplitude},
+        {"feedback", warper["aliased"]},
+    });
 }
 
 IV_EXPORT_MODULE("iv.test.noisy_saw_voice", noisy_saw_voice);
