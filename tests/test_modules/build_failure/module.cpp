ae12659614acc0a10b91f216ba1f2e55fa575570
ljleb <lebel.louisjacob@gmail.com>
2026-08-21 09:55:47 -0400

    Keep build failure fixture on new module signature

--- tests/test_modules/build_failure/module.cpp
+++ tests/test_modules/build_failure/module.cpp
@@ -1,13 +1,10 @@
 #include <intravenous/dsl.h>
 
 namespace {
-    void build_failure(iv::ModuleContext const& context)
-    {
-        auto& g = context.builder();
-        g.outputs();
-    }
+void build_failure(iv::GraphBuilder& g)
+{
+    g.outputs();
 }
-
-IV_EXPORT_MODULE("iv.test.build_failure", build_failure);
+}
 
 this will not compile
