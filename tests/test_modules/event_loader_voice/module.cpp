ae512b3dd1a48f2c76f4877404ccd66db8934185
ljleb <lebel.louisjacob@gmail.com>
2026-08-21 07:02:38 -0400

    Migrate event voice to GraphBuilder entry

--- tests/test_modules/event_loader_voice/module.cpp
+++ tests/test_modules/event_loader_voice/module.cpp
@@ -1,13 +1,9 @@
 #include <intravenous/dsl.h>
 
-inline void event_loader_voice(iv::ModuleContext const& context)
+inline void event_loader_voice(iv::GraphBuilder& g)
 {
     using namespace iv;
-    auto& g = context.builder();
-
     auto const trigger = g.event_input<"trigger">(EventTypeId::trigger);
     g.event_outputs("trigger"_F = trigger);
     g.outputs();
 }
-
-IV_EXPORT_MODULE("iv.test.event_loader_voice", event_loader_voice);
