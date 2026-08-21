5f5df15349a5aa40c5cf2b2469d5b1adac71cab1
ljleb <lebel.louisjacob@gmail.com>
2026-08-21 07:02:52 -0400

    Migrate reload voice to GraphBuilder entry

--- tests/test_modules/reload_voice/module.cpp
+++ tests/test_modules/reload_voice/module.cpp
@@ -1,24 +1,14 @@
 #include <intravenous/dsl.h>
 #include <intravenous/basic_nodes/shaping.h>
 
-inline void reload_voice(iv::ModuleContext const& context)
+inline void reload_voice(iv::GraphBuilder& g)
 {
     using namespace iv;
-    auto& g = context.builder();
-
     auto const amplitude = g.input<"amplitude">(0.1);
     auto const frequency = g.input<"frequency">(220.0);
     auto const phase_offset = g.input<"phase_offset">(0.0);
     auto const dt = g.input<"dt">(1.0);
-
     auto const saw = g.node<SawOscillator>();
-    saw(
-        "frequency"_P = frequency,
-        "phase_offset"_P = phase_offset,
-        "dt"_P = dt
-    );
-
+    saw("frequency"_P = frequency, "phase_offset"_P = phase_offset, "dt"_P = dt);
     g.outputs("out"_P = saw * amplitude);
 }
-
-IV_EXPORT_MODULE("iv.test.reload_voice", reload_voice);
