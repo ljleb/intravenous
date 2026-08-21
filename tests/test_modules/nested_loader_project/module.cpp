607cb5973d3060875c1a54473d36a9692bada680
ljleb <lebel.louisjacob@gmail.com>
2026-08-21 07:02:31 -0400

    Use generated import in nested loader project

--- tests/test_modules/nested_loader_project/module.cpp
+++ tests/test_modules/nested_loader_project/module.cpp
@@ -1,26 +1,17 @@
 #include <intravenous/dsl.h>
 #include <intravenous/basic_nodes/buffers.h>
 #include <intravenous/basic_nodes/shaping.h>
+#include <iv/modules/iv.test.nested_loader_voice>
 
 inline void nested_loader_project(iv::ModuleContext const& context)
 {
     using namespace iv;
     auto& g = context.builder();
     auto const dt = g.node<ValueSource>(&context.sample_period());
-
-    auto const voice_builder = context.load_builder("iv.test.nested_loader_voice");
     auto const phase = g.node<PhaseIntegrator>();
-    auto const voice = g.embed_subgraph(voice_builder);
-
+    auto const voice = g.module<&nested_loader_voice>();
     phase(0.0);
-    auto const tone = voice(
-        "amplitude"_P = 0.25,
-        "frequency"_P = 220.0,
-        "phase_offset"_P = phase,
-        "dt"_P = dt
-    );
-
+    auto const tone = voice("amplitude"_P = 0.25, "frequency"_P = 220.0,
+        "phase_offset"_P = phase, "dt"_P = dt);
     g.outputs("main"_P[stereo::left] = tone, "main"_P[stereo::right] = tone);
 }
-
-IV_EXPORT_MODULE("iv.test.nested_loader_project", nested_loader_project);
