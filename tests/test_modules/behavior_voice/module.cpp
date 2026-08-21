2698212deceefa1bc64cc47fa6dd03429621d6b3
ljleb <lebel.louisjacob@gmail.com>
2026-08-21 09:49:21 -0400

    Remove graph-level sample period from behavior voice

--- tests/test_modules/behavior_voice/module.cpp
+++ tests/test_modules/behavior_voice/module.cpp
@@ -7,8 +7,7 @@ inline void behavior_voice(iv::GraphBuilder& g)
     auto const amplitude = g.input<"amplitude">(0.1);
     auto const frequency = g.input<"frequency">(220.0);
     auto const phase_offset = g.input<"phase_offset">(0.0);
-    auto const dt = g.input<"dt">(1.0);
     auto const saw = g.node<SawOscillator>();
-    saw("frequency"_P = frequency, "phase_offset"_P = phase_offset, "dt"_P = dt);
+    saw("frequency"_P = frequency, "phase_offset"_P = phase_offset);
     g.outputs("out"_P = saw * amplitude);
 }
