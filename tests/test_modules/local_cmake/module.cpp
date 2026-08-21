a313749cd4d43c864f40f56f7b2d0bd18b5af37e
ljleb <lebel.louisjacob@gmail.com>
2026-08-21 09:33:23 -0400

    Migrate local CMake fixture to GraphBuilder module

--- tests/test_modules/local_cmake/module.cpp
+++ tests/test_modules/local_cmake/module.cpp
@@ -1,18 +1,11 @@
 #include <intravenous/dsl.h>
-#include <intravenous/basic_nodes/buffers.h>
 
-namespace {
-    using iv::operator""_P;
+using iv::operator""_P;
 
-    void local_cmake_module(iv::ModuleContext const& context)
-    {
-        auto& g = context.builder();
-        auto const dt = g.node<iv::ValueSource>(&context.sample_period());
-        g.outputs(
-            "main"_P[iv::stereo::left] = dt * 0.0f,
-            "main"_P[iv::stereo::right] = dt * 0.0f
-        );
-    }
+void local_cmake_module(iv::GraphBuilder& g)
+{
+    g.outputs(
+        "main"_P[iv::stereo::left] = 0.0f,
+        "main"_P[iv::stereo::right] = 0.0f
+    );
 }
-
-IV_EXPORT_MODULE("iv.test.local_cmake", local_cmake_module);
