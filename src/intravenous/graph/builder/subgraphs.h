fd758cfc9f99c1bca229c4590a44816d28b0caa6
ljleb <lebel.louisjacob@gmail.com>
2026-08-24 18:44:07 -0400

    phase4: make SubgraphBuilder authoring constexpr

--- src/intravenous/graph/builder/subgraphs.h
+++ src/intravenous/graph/builder/subgraphs.h
@@ -20,12 +20,13 @@ class SubgraphBuilder {
   GraphBuilder& _builder;
   GraphBuilderPublicPorts _ports;
 
-  explicit SubgraphBuilder(GraphBuilder&, NodeBundleHandle);
+  constexpr explicit SubgraphBuilder(GraphBuilder&, NodeBundleHandle);
 
-  PublicSampleInputRef input_named(std::string_view name, Sample default_value,
-                                   std::optional<Sample> min,
-                                   std::optional<Sample> max);
-  PublicEventInputRef event_input_named(std::string_view name, EventTypeId type);
+  constexpr PublicSampleInputRef input_named(
+      std::string_view name, Sample default_value,
+      std::optional<Sample> min, std::optional<Sample> max);
+  constexpr PublicEventInputRef event_input_named(
+      std::string_view name, EventTypeId type);
 
 public:
   SubgraphBuilder(SubgraphBuilder const&) = delete;
@@ -33,31 +34,33 @@ public:
   SubgraphBuilder(SubgraphBuilder&&) = delete;
   SubgraphBuilder& operator=(SubgraphBuilder&&) = delete;
 
-  PublicSampleInputRef input();
+  constexpr PublicSampleInputRef input();
   template<fixed_string Name>
-  PublicSampleInputRef input(Sample default_value = 0.0,
-                             std::optional<Sample> min = std::nullopt,
-                             std::optional<Sample> max = std::nullopt) {
+  constexpr PublicSampleInputRef input(
+      Sample default_value = 0.0,
+      std::optional<Sample> min = std::nullopt,
+      std::optional<Sample> max = std::nullopt) {
     return input_named(Name.view(), default_value, min, max);
   }
-  PublicSampleInputRef input(Sample default_value,
-                             std::optional<Sample> min = std::nullopt,
-                             std::optional<Sample> max = std::nullopt);
+  constexpr PublicSampleInputRef input(
+      Sample default_value,
+      std::optional<Sample> min = std::nullopt,
+      std::optional<Sample> max = std::nullopt);
 
   template<fixed_string Name>
-  PublicEventInputRef event_input(EventTypeId type) {
+  constexpr PublicEventInputRef event_input(EventTypeId type) {
     return event_input_named(Name.view(), type);
   }
-  PublicEventInputRef event_input(EventTypeId type);
+  constexpr PublicEventInputRef event_input(EventTypeId type);
 
   template<class... Refs>
-  void event_outputs(Refs&&... refs);
-  void event_outputs(std::span<EventOutputRefConfig const> refs);
+  constexpr void event_outputs(Refs&&... refs);
+  constexpr void event_outputs(std::span<EventOutputRefConfig const> refs);
 
   template<class... Refs>
-  void outputs(Refs&&... refs);
+  constexpr void outputs(Refs&&... refs);
   constexpr void outputs(std::initializer_list<NamedRef> refs);
-  void outputs(std::span<OutputRefConfig const> refs);
+  constexpr void outputs(std::span<OutputRefConfig const> refs);
   constexpr void outputs(std::span<NamedRef const> refs);
 };
 
