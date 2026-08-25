6a83bf309a65fe799fed6cebd9942c1d68b976b7
ljleb <lebel.louisjacob@gmail.com>
2026-08-24 20:38:11 -0400

    phase4: unify static storage header

--- src/intravenous/graph/static_storage.hpp
+++ src/intravenous/graph/static_storage.hpp
@@ -1,15 +1,113 @@
 #pragma once
 
-#include <intravenous/graph/static_storage.h>
+#include <intravenous/ports.h>
 
 #include <meta>
 
+#include <cstddef>
+#include <limits>
 #include <ranges>
+#include <span>
+#include <string>
 #include <string_view>
 #include <type_traits>
 #include <vector>
 
-namespace iv::details {
+namespace iv {
+template<class T>
+struct StaticSpan {
+    T const* data = nullptr;
+    size_t size = 0;
+
+    constexpr T const* begin() const { return data; }
+    constexpr T const* end() const { return data + size; }
+    constexpr bool empty() const { return size == 0; }
+    constexpr T const& front() const { return data[0]; }
+    constexpr T const& back() const { return data[size - 1]; }
+    constexpr T const& operator[](size_t index) const { return data[index]; }
+    constexpr std::span<T const> span() const { return { data, size }; }
+};
+
+struct StaticString {
+    char const* data = nullptr;
+    size_t size = 0;
+
+    constexpr bool empty() const { return size == 0; }
+    constexpr std::string_view view() const { return { data, size }; }
+    constexpr operator std::string_view() const { return view(); }
+};
+
+struct StaticInputConfig {
+    StaticString name {};
+    ChannelLayout channel_layout {
+        .channel_type = ChannelTypeId::mono,
+        .sample_layout = SampleStreamLayout::planar,
+    };
+    size_t history = 0;
+    Sample default_value = 0.0;
+    Sample min = -std::numeric_limits<Sample::storage>::infinity();
+    Sample max = std::numeric_limits<Sample::storage>::infinity();
+
+    constexpr InputConfig config() const
+    {
+        return {
+            .name = std::string(name.view()),
+            .channel_layout = channel_layout,
+            .history = history,
+            .default_value = default_value,
+            .min = min,
+            .max = max,
+        };
+    }
+};
+
+struct StaticOutputConfig {
+    StaticString name {};
+    ChannelLayout channel_layout {
+        .channel_type = ChannelTypeId::mono,
+        .sample_layout = SampleStreamLayout::planar,
+    };
+    size_t latency = 0;
+    size_t history = 0;
+
+    constexpr OutputConfig config() const
+    {
+        return {
+            .name = std::string(name.view()),
+            .channel_layout = channel_layout,
+            .latency = latency,
+            .history = history,
+        };
+    }
+};
+
+struct StaticEventInputConfig {
+    StaticString name {};
+    EventTypeId type {};
+
+    constexpr EventInputConfig config() const
+    {
+        return {
+            .name = std::string(name.view()),
+            .type = type,
+        };
+    }
+};
+
+struct StaticEventOutputConfig {
+    StaticString name {};
+    EventTypeId type {};
+
+    constexpr EventOutputConfig config() const
+    {
+        return {
+            .name = std::string(name.view()),
+            .type = type,
+        };
+    }
+};
+
+namespace details {
 template<std::ranges::contiguous_range Range>
 consteval auto define_static_span(Range const& range)
 {
@@ -79,4 +177,5 @@ consteval StaticSpan<StaticConfig> define_static_configs(
     }
     return define_static_span(static_configs);
 }
-}
+} // namespace details
+} // namespace iv
