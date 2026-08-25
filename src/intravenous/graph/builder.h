defb2ab7b8f32e2727cb78fd45824b6639ffc6da
ljleb <lebel.louisjacob@gmail.com>
2026-08-24 20:34:25 -0400

    phase4: complete constexpr builder definitions in header

--- src/intravenous/graph/builder.h
+++ src/intravenous/graph/builder.h
@@ -203,6 +203,78 @@ private:
   constexpr SamplePortRef lift_to_sample_port(NamedRef const& ref);
 };
 
+constexpr SubgraphBuilder::SubgraphBuilder(
+    GraphBuilder& builder, NodeBundleHandle boundary)
+    : _builder(builder), _ports(boundary) {}
+
+constexpr PublicSampleInputRef SubgraphBuilder::input() {
+  return input(Sample{0.0f});
+}
+
+constexpr PublicSampleInputRef SubgraphBuilder::input_named(
+    std::string_view name, Sample value, std::optional<Sample> min,
+    std::optional<Sample> max) {
+  return PublicSampleInputRef(
+      _ports.add_sample_input(_builder, _builder._node_bundles,
+                              name, value, min, max));
+}
+
+constexpr PublicSampleInputRef SubgraphBuilder::input(
+    Sample value, std::optional<Sample> min, std::optional<Sample> max) {
+  return PublicSampleInputRef(
+      _ports.add_sample_input(_builder, _builder._node_bundles,
+                              {}, value, min, max));
+}
+
+constexpr PublicEventInputRef SubgraphBuilder::event_input_named(
+    std::string_view name, EventTypeId type) {
+  return PublicEventInputRef(
+      _ports.add_event_input(_builder, _builder._node_bundles, name, type));
+}
+
+constexpr PublicEventInputRef SubgraphBuilder::event_input(EventTypeId type) {
+  return PublicEventInputRef(
+      _ports.add_event_input(_builder, _builder._node_bundles, {}, type));
+}
+
+constexpr void SubgraphBuilder::event_outputs(
+    std::span<EventOutputRefConfig const> refs) {
+  _ports.define_event_outputs(
+      _builder, _builder._node_bundles, _builder._identity, refs);
+}
+
+constexpr void SubgraphBuilder::outputs(std::initializer_list<NamedRef> refs) {
+  outputs(std::span<NamedRef const>(refs.begin(), refs.size()));
+}
+
+constexpr void SubgraphBuilder::outputs(std::span<OutputRefConfig const> refs) {
+  _ports.define_sample_outputs(
+      _builder, _builder._node_bundles, _builder._identity, refs);
+}
+
+constexpr void SubgraphBuilder::outputs(std::span<NamedRef const> refs) {
+  _ports.define_sample_outputs_from_named_refs(
+      _builder, _builder._node_bundles, _builder._identity,
+      [&](auto&& value) {
+        return _builder.lift_to_sample_port(
+            std::forward<decltype(value)>(value));
+      },
+      refs);
+}
+
+constexpr void GraphBuilder::outputs(std::initializer_list<NamedRef> refs) {
+  outputs(std::span<NamedRef const>(refs.begin(), refs.size()));
+}
+
+constexpr void GraphBuilder::outputs(std::span<NamedRef const> refs) {
+  _public_ports.define_sample_outputs_from_named_refs(
+      *this, _node_bundles, _identity,
+      [&](auto&& value) {
+        return lift_to_sample_port(std::forward<decltype(value)>(value));
+      },
+      refs);
+}
+
 constexpr void GraphBuilderPublicPorts::define_sample_outputs(
     GraphBuilder& builder, GraphBuilderNodeBundles& bundles,
     GraphBuilderIdentity const& identity, std::span<OutputRefConfig const> refs) {
